4b51a38bf22401cceb0c92df177431c122f86969
IlyaOriekhov <164690908+IlyaOriekhov@users.noreply.github.com>
2024-11-13 16:50:33 +0200

    type data

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,3 +1,6 @@
 function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
+
+const mergedObject = merge({ name: "Edward" }, { age: 22 });
+console.log(mergedObject);
